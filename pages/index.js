6090a7ecfe67bed4a4e2b8df319ddc4ea53e6765
yotink522 <yotink522@hotmail.com>
2022-03-02 22:25:27 +0800

    Route Product Link

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ import {
   CardActions,
   Button,
 } from '@material-ui/core';
+import NextLink from 'next/link';
 import Layout from '../components/Layout';
 import data from '../utils/data';
 
@@ -25,16 +26,18 @@ export default function Home() {
           {data.products.map((product) => (
             <Grid item md={4} key={product.name}>
               <Card>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    image={product.image}
-                    title={product.name}
-                  ></CardMedia>
-                  <CardContent>
-                    <Typography>{product.name}</Typography>
-                  </CardContent>
-                </CardActionArea>
+                <NextLink href={`/product/${product.slug}`} passHref>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      title={product.name}
+                    ></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </NextLink>
                 <CardActions>
                   <Typography>${product.price}</Typography>
                   <Button size="small" color="primary">
