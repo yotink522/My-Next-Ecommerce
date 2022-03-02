ad5de418f4cb7cfe046359ea56f12071d17e6fca
yotink522 <yotink522@hotmail.com>
2022-03-02 16:17:30 +0800

    add utils/styles.js to make sure css effect

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import Head from 'next/head';
 import { AppBar, Typography, Toolbar, Container } from '@material-ui/core';
+import useStyles from '../utils/styles';
 
 export default function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>My Ecommerce</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>My EC</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-
-      <footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
         <Typography>All right reserved. Next Ecommerce</Typography>
       </footer>
     </div>
